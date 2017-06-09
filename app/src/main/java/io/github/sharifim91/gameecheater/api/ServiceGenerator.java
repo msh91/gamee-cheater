9b4cc8cbdc4a48a7929d0ca9efcb9f687d783ad2
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2017-06-09 23:21:21 +0430

    add retrofit lib and create api service interface

---
+++ app/src/main/java/io/github/sharifim91/gameecheater/api/ServiceGenerator.java
@@ -0,0 +1,98 @@
+package io.github.sharifim91.gameecheater.api;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Converter;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by User1 on 10/3/2016.
+ */
+
+public final class ServiceGenerator {
+    private static final String TAG = ServiceGenerator.class.getSimpleName();
+    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+    private static final Gson gson = new GsonBuilder()
+            .setDateFormat(DATE_FORMAT)
+            .create();
+    private static final Retrofit.Builder retrofit = new Retrofit.Builder()
+            .baseUrl("https://bots.gameeapp.com")
+            .addConverterFactory(buildConverterFactory());
+
+    private static Converter.Factory buildConverterFactory() {
+        GsonBuilder gsonBuilder = new GsonBuilder()
+                .setDateFormat(DATE_FORMAT);
+
+        return GsonConverterFactory.create(gsonBuilder.create());
+    }
+
+    /**
+     * Create service for using in other class
+     * Accept-Encoding: gzip
+     *
+     * @param serviceClass
+     * @param <S>
+     * @return
+     */
+    public static <S> S createService(Class<S> serviceClass, String token) {
+        final OkHttpClient build = new OkHttpClient
+                .Builder()
+                .readTimeout(20, TimeUnit.SECONDS)
+                .addInterceptor(new LoggerInterceptor(token))
+                .build();
+
+        return retrofit.client(build).build().create(serviceClass);
+    }
+
+
+    /**
+     * Just for calculate nano time of request sending and response receiving
+     * and implementation of {@link Interceptor}
+     */
+    private static class LoggerInterceptor implements Interceptor {
+
+        private final String token;
+
+        public LoggerInterceptor(String token) {
+            if (!TextUtils.isEmpty(token)) {
+                this.token = "Token " + token;
+            } else {
+                this.token = "";
+            }
+        }
+
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            final long t1 = System.nanoTime();
+            final Request original = chain.request();
+            final Request.Builder requestBuilder = original.newBuilder();
+            final Request request = requestBuilder
+                    .url(original.url())
+                    .header("Accept-Language", "fa")
+                    .header("Authorization", token)
+                    .method(original.method(), original.body()).build();
+
+            Log.d(TAG, String.format("Sending Request %s on %s%n%s ",
+                    request.url(), chain.connection(), request.headers()));
+            final Response response = chain.proceed(requestBuilder.build());
+            long t2 = System.nanoTime();
+            Log.d(TAG, String.format("Received Response for %s in %.1fms %n %s",
+                    response.request().url(), (t2 - t1) / 1e6d, response.body()
+            ));
+            return response;
+        }
+    }
+
+}
